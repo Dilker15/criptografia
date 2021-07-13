4d8d322ba1e291970fe62254d40a20a8ff83ddcf
Herbert LF <herbert.code@gmail.com>
2021-07-12 23:51:58 -0400

    Cifrado por sustitucion monoalfabetica

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Criptografía Clásica</title>
+</head>
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                Criptografía Clásica
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">Proyecto para Criptografía y Seguridad - UAGRM</h5>
+                <p class="card-text">Content</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <div class="card-header bg-dark text-light">                        
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Cifra de Desplazamiento</h5>
+                        <p class="card-text">Este cifrado se basa principalmente en desplazamientos sobre un alfabeto.</p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="desplazamiento">
+                            <button type="button" class="btn btn-dark">Ingresar</button>
+                        </a>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="col">
+                <div class="card">
+                    <div class="card-header bg-warning text-dark">                        
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Cifra por Transposición</h5>
+                        <p class="card-text"> método de cifra que consiste en cambiar de lugar los elementos del texto en claro en el criptrograma</p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="transposicion">
+                            <button type="button" class="btn btn-warning">Ingresar</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <div class="card-header bg-danger text-dark">                        
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Sustitución</h5>
+                        <p class="card-text">son los que han tenido un mayor desarrollo en la historia de la criptografía clásica</p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="sustitucion">
+                            <button type="button" class="btn btn-danger">Ingresar</button>
+                        </a>                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ librerias/CifradoSustitucion.php
@@ -0,0 +1,145 @@
+<?php
+
+    class CifradoSustitucion {
+
+        private $alfabeto = [
+            0  => 'A',
+            1  => 'B',
+            2  => 'C',
+            3  => 'D',
+            4  => 'E',
+            5  => 'F',
+            6  => 'G',
+            7  => 'H',
+            8  => 'I',
+            9  => 'J',
+            10 => 'K', 
+            11 => 'L',
+            12 => 'M',
+            13 => 'N',
+            14 => 'Ñ',
+            15 => 'O',
+            16 => 'P',
+            17 => 'Q',
+            18 => 'R',
+            19 => 'S',
+            20 => 'T',
+            21 => 'U',
+            22 => 'V',
+            23 => 'W',
+            24 => 'X',
+            25 => 'Y',
+            26 => 'Z'
+        ];
+
+        private $alfabeto_sustitucion = [];
+        private $alfabeto_invertido = [];
+
+        public $texto_claro;
+        public $texto_cifrado;
+        private $tipo_seleccionado;
+        private $tipos = [
+            "0" => "Mono alfabética",
+            "1" => "Poli alfabética",
+        ];
+
+        function __construct() {
+        }
+
+        public function getTipoCifrado($tipo) {
+            if($this->getTipoCifrado($tipo)){
+                return $this->tipos[$this->tipo_seleccionado];
+            }
+        }
+
+        public function setTipoCifrado($tipo) {
+            switch ($tipo) {
+                case 0: 
+                    $this->tipo_seleccionado = $this->tipos[$tipo];
+                    break;
+                case 1: 
+                    $this->tipo_seleccionado = $this->tipos[$tipo];
+                    break;
+                default:                
+                    return false;
+            }
+        }
+
+        public function getAlfabeto(){
+            return $this->alfabeto;
+        }
+
+        private function setAlfabetoInvertido(){
+            $cantidad_letras = count($this->alfabeto);
+            $j = $cantidad_letras-1;
+            for ($i=0; $i < $cantidad_letras; $i++) { 
+                $this->alfabeto_invertido[$j] = $this->alfabeto[$i];
+                $j--;
+            }
+        }
+
+        public function getAlfabetoInvertido(){
+            if($this->alfabeto_invertido == null ){
+                $this->setAlfabetoInvertido();                
+            } 
+            return $this->alfabeto_invertido;
+        }
+
+        public function cifrar(){
+            switch ($this->tipo_seleccionado) {
+                case $this->tipos[0]:
+                    $this->cifrarAlfabeticamente();
+                    break;
+                case $this->tipos[1]:
+                    $this->cifrarPoliAlfabeticamente();
+                    break;
+                    
+                default:
+                    # code...
+                    break;
+            }
+        }
+
+        public function cifrarAlfabeticamente($texto_claro, $alfabeto){
+            $texto_cifrado = "";
+            $texto_claro = strtoupper($texto_claro);
+            $longitud_texto = strlen($texto_claro);
+            for($i=0; $i < $longitud_texto ;$i++){                
+                $letra_tc = $texto_claro[$i];
+                echo $letra_tc . "\n";
+                echo "Encoded " . utf8_encode($letra_tc) . " )";
+                echo "Decoded " . utf8_decode($letra_tc) . "";
+                $indice = array_search($letra_tc, $this->alfabeto);
+                if($indice !== false){
+                    $texto_cifrado .= $alfabeto[$indice];
+                }
+            }
+            return $texto_cifrado;
+        }
+
+        public function descifrarAlfabeticamente($texto_encriptado, $alfabeto){
+            $texto_claro = "";
+            $texto_encriptado = strtoupper($texto_encriptado);
+            $longitud_texto = strlen($texto_encriptado);
+            for($i=0; $i < $longitud_texto ;$i++){                
+                $letra_tc = $texto_encriptado[$i];
+               
+                $indice = array_search($letra_tc, $alfabeto);
+                if($indice !== false){
+                    $texto_claro .= $this->alfabeto[$indice];
+                }
+            }
+            return $texto_claro;
+        }
+
+        public function cifrarPoliAlfabeticamente(){
+            
+        }
+
+
+        function __destruct() {
+            
+        }
+
+    }
+
